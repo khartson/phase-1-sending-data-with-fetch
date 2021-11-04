5b8262b1ab7063f7d33c9f06f986d5584b5cbdf2
Kyle <khartson2017@gmail.com>
2021-11-04 17:12:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(firstName, emaiAd) { 
+  const formData = {
+      name: `${firstName}`,
+      email: `${emaiAd}`,
+  }
+
+  const configure = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configure)
+  .then(function (response) {
+      return response.json();
+  })
+  .then (function (object) {
+      let h1 = document.createElement('h1');
+      document.body.innerHTML = object['id'];
+  })
+  .catch(function (error) {
+      document.body.innerHTML += error.message; 
+  });
+};
